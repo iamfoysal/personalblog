d89e9e784d442069f788ee37d74e3c3d0a14e740
foysal <foysalf652@gmail.com>
2021-08-26 00:04:02 +0600

    last eddit eid

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class post(models.model):
+    title =  models.CharField(max_length=100)
+    content = models.
