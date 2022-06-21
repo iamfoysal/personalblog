1ffe13267ac3ff0be1472fe95bc50fbd73695f3f
iamfoysal <foysalf652@gmail.com>
2022-06-21 12:43:03 +0600

    profile update

--- django_project/urls.py
+++ django_project/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     # path('account_update/', user_views.account_update, name='account_update'),
     path('register/', user_views.register, name= 'register'),
-    # path('profile/', user_views.profile, name= 'profile'),
+    path('profile/', user_views.profile, name= 'profile'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name= 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name= 'logout'),  
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/forms.py
+++ user/forms.py
@@ -1,10 +1,31 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     class Meta: 
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name','about','location','image']
+        # fields = ['image']
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+
+    #     for name, field in self.fields.items():
+    #         field.widget.attrs.update({'class': 'form-control'})
+    
